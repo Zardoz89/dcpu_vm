e7a7e98e791a52455bb8b861d239c8d702cc341f
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-08-18 23:10:02 +0200

    Created a basic dissambler to see what happend exactly in each instrucction execution

---
+++ disassembler.hpp
@@ -0,0 +1,19 @@
+#ifndef DISASSEMBLER_HPP
+#define DISASSEMBLER_HPP 1
+
+#include <string>
+#include <cstdint>
+
+namespace cpu
+{
+
+/**
+ * Disassembly DCPU-16 code to a humman redable text
+ * @param data Ptr. to dcpu-16 machine code
+ * @param size Size of data
+ */
+std::string disassembly(const uint16_t* data, size_t size);
+
+}
+
+#endif // DISASSEMBLER_HPP
