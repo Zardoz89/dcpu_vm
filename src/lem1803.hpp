1ae774bd96815212a400f3749132b7551e5a5e06
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-08-26 18:50:32 +0200

    Implementing

---
+++ src/lem1803.hpp
@@ -0,0 +1,97 @@
+#ifndef _LEM1803_HPP
+#define _LEM1803_HPP
+
+#include <iostream>
+#include <cstdint>
+
+#include "dcpu.hpp" // Base class: cpu::IHardware
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+
+#define MEM_MAP_SCREEN   0
+#define MEM_MAP_FONT     1
+#define MEM_MAP_PALETTE  2
+#define SET_BORDER_COLOR 3
+#define MEM_DUMP_FONT    4
+#define MEM_DUMP_PALETTE 5
+
+namespace cpu {
+
+const int FPS               = 30;
+const unsigned int WIDTH    = 128;
+const unsigned int HEIGHT   = 96;
+
+const unsigned int ROWS     = 12;
+const unsigned int COLS     = 32;
+
+const uint16_t BLINKRATE    = 10000; // Change Blink state each N ticks
+
+/**
+ * @brief LEM1802 that uses SFML
+ */
+class Lem1803 : public cpu::IHardware {
+public:
+    Lem1803();
+    virtual ~Lem1803();
+    
+    static const uint32_t id            = 0x7349f615;
+    static const uint16_t revision      = 0x1802;
+    static const uint32_t manufacturer  = 0x1c6c8b36;
+    virtual uint32_t getId() {
+        return id;
+    }
+    virtual uint16_t getRevision() {
+        return revision;
+    }
+    virtual uint32_t getManufacturer() {
+        return manufacturer;
+    }
+    
+    bool checkInterrupt (uint16_t &msg) {
+        return false;
+    }
+    virtual void handleInterrupt();
+    virtual void tick();
+    
+    virtual void attachTo (DCPU* cpu, size_t index);
+    
+    /**
+     * @brief Try to show the screen in the terminal
+     */
+    void show();
+
+    /**
+     * @brief Sets if it can display to stdout
+     */
+    void setEnable(bool enable);
+
+    const static uint16_t def_palette_map[16];   /// Default palette
+    const static uint16_t def_font_map[128*2];    /// Default fontmap
+
+private:
+
+    uint16_t screen_map;            /// Where map VIDEO RAM 
+    uint16_t font_map;              /// Where map FONT
+    uint16_t palette_map;           /// Where map PALETTE
+    
+    uint8_t border_col;             /// Border color (unused)
+    uint32_t ticks;                 /// CPU clock ticks (for timing)
+    
+    uint32_t tick_per_refresh;      /// How many ticks before refresh
+
+    bool enable;                    /// Can print to stdout ?
+
+    uint16_t blink;                 /// Counter for blinking
+   
+    sf::RenderWindow window;        /// SFML window
+    sf::Texture texture;            /// SFML texture were to paint
+
+
+};
+
+}
+
+#endif // _LEM1802_HPP
