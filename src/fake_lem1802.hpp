c33951b77a066723c92ea06346cc828893c75451
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-08-26 18:13:49 +0200

    Moved default font and default palette to static const arrays

--- src/fake_lem1802.hpp
+++ src/fake_lem1802.hpp
@@ -53,7 +53,10 @@ public:
      * @brief Sets if it can display to stdout
      */
     void setEnable(bool enable);
-    
+   
+    const static uint16_t def_palette_map[16];   /// Default palette
+    const static uint16_t def_font_map[128*2];   /// Default fontmap
+
 private:
 
     uint16_t screen_map;            /// Where map VIDEO RAM 
@@ -61,8 +64,6 @@ private:
     uint16_t palette_map;           /// Where map PALETTE
     
     uint8_t border_col;             /// Border color (unused)
-    const uint16_t *def_font_map;   /// Default font map
-    uint16_t def_palette_map[16];   /// Default palette
     uint32_t ticks;                 /// CPU clock ticks (for timing)
     
     uint32_t tick_per_refresh;      /// How many ticks before refresh
