fc2f72eabc9e9c6b18c5b462544742ef10d2ce3f
Luis Panadero Guardeño <luis.panadero@gmail.com>
2013-09-10 17:48:01 +0200

    Updated floppy interface with recopiled data of device speed writing/reading data on every sector

--- include/devices/m35fd.hpp
+++ include/devices/m35fd.hpp
@@ -2,14 +2,14 @@
 #define __M35FD__HPP__ 1
 
 #include <dcpu.hpp>
+#include <config.hpp>
 
 #include <cstdint>
+#include <iostream>
 
 namespace cpu {
 
 namespace m35fd {
-
-// TODO Find information about the real write/read speed
 
 /**
  * M35 Floppy Drive commands
@@ -48,6 +48,27 @@ enum class ERROR_CODES : uint16_t {
 class M35_Floppy;
 
 /**
+ * How many cycles need to move to a neraby track
+ */
+static const unsigned SEEK_CYCLES_PER_TRACK     = 240;
+
+/**
+ * How many cycles need to read/write a whole sector
+ */
+static const unsigned READ_CYCLES_PER_SECTOR    = 1668;
+static const unsigned WRITE_CYCLES_PER_SECTOR   = 1668;
+
+/**
+ * How many sectors are in a track
+ */
+static const unsigned SECTORS_PER_TRACK         = 18;
+
+/**
+ * How many words are stored in a sector
+ */
+static const unsigned SECTOR_SIZE               = 512;
+
+/**
  * Mackapar 3,5" floppy drive
  */
 class M35FD : public IHardware {
@@ -55,9 +76,21 @@ public:
     M35FD();
     virtual ~M35FD();
 
-    uint32_t getId()                { return 0x4FD524C5; }
-    virtual uint16_t getRevision()  { return 0x000B; }
-    uint32_t getManufacturer()      { return 0x1EB37E91; } // MACKAPAR
+    uint32_t getId() const               
+    { 
+        return 0x4FD524C5; 
+    }
+
+    virtual uint16_t getRevision() const 
+    { 
+        return 0x000B; 
+    }
+
+    uint32_t getManufacturer() const     
+    { 
+        return 0x1EB37E91;  // MACKAPAR
+
+    }
 
     virtual void attachTo (DCPU* cpu, size_t index);
 
@@ -87,11 +120,87 @@ protected:
  */
 class M35_Floppy {
 public:
-    M35_Floppy();
+
+    /**
+     * Creates a new floppy device
+     */
+    M35_Floppy(const std::string filename , uint8_t tracks = 80);
     virtual ~M35_Floppy();
 
+    /**
+     * Total number of tracks of this floppy
+     */
+    uint8_t getTotalTracks() const;
+    
+    /**
+     * Total number of sectors of this floppy
+     */
+    uint16_t getTotalSectors() const;
+
+    /**
+     * Gets the actual Track
+     */
+    uint16_t getTrack() const;
+   
+    /**
+     * See if that sector is bad
+     * Return True if is a bad sector
+     */
+    bool isSectorBad (uint16_t sector) const;
+
+    /**
+     * Change the bad sector flag of a particular sector
+     * @param sector Desired sector
+     * @param state True to damage these particular sector
+     */
+    void setSectorBad (uint16_t sector, bool state);
+
+    /**
+     * Try to write data at the desired sector
+     * @param sector Desired sector to be writed
+     * @param cycles Number of cycles that takes to finish the operation
+     * @param data Data to be written in the sector
+     * @return NONE, PROTECTED or BAD_SECTOR
+     */
+    ERROR_CODES write (uint16_t sector, unsigned& cycles, const uint16_t* data,
+                       size_t size);
+    
+    /**
+     * Try to read data at the desired sector
+     * @param sector Desired sector to be writed
+     * @param cycles Number of cycles that takes to finish the operation
+     * @param data Buffer were to write the data
+     * @return NONE or BAD_SECTOR
+     */
+    ERROR_CODES readS (uint16_t sector, unsigned& cycles, uint16_t* data, 
+                       size_t size);
 
 protected:
+    // Note, this vars, are what will be write to the disk file in real world
+    // To find a particular sector we use this :
+    // ptr_word = sector * SECTORS_SIZE
+
+    // File structure should be like this:
+    // Byte : Number of tracks
+    // RAW Data chunk of Tracks * SECTORS_PER_TRACK size
+    // Bitmap of bad sectors of (Tracks * SECTORS_PER_TRACK) / 8
+    //
+    // The bitmap stores 8 sectors state in a byte. It uses the MSB bit for the
+    // lowest sector and LSB for the bigger sector.
+    //
+    // The RAW data will be read/write directly to the file, butthe bitmap will
+    // be keep in RAM for quick read of it.
+
+    uint8_t tracks; /// Total tracks of the floppy
+    uint8_t* bad_sectors; /// Bitmap of bad sectors
+
+    std::iostream& datafile;
+
+    /**
+     * Moves the head to the desired track
+     * @retun Number of cycles that need to seek the desired track
+     */
+    unsigned setTrack(uint16_t track);
 };
 
 } // END OF NAMESPACE block_device
