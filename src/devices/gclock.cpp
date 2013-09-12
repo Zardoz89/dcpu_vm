1edc011dd9c820634091a3b384919f4f16bdd8ec
Julien <julien.deloor@gmail.com>
2013-09-12 20:57:41 +0200

    Fixed generic clock  bugged on windows
    problem : the accumation sometimes accumulate 0 instead of 1 or 2 (cause
    the time resolution on windows is not accurate and the refresh is too
    speed and the clock is restarted. I fixed that, it's not beautifull but
    that's should work.

--- src/devices/gclock.cpp
+++ src/devices/gclock.cpp
@@ -5,7 +5,7 @@
 namespace cpu {
 
 Generic_Clock::Generic_Clock() : 
-    ticks(0), msg(0), trigger(false), acum(0), max_acum(0), divider(0) 
+    ticks(0), msg(0), trigger(0), acum(0), max_acum(0), divider(0) 
 {
     clock.restart();
 }
@@ -23,10 +23,12 @@ unsigned Generic_Clock::handleInterrupt()
     case 0:
         divider = cpu->getB();
         if (cpu->getB() > 0) {
-            this->max_acum =  sf::seconds(60.0f / divider).asMicroseconds();
+            this->max_acum =  60000 / divider;
+            //this->max_acum =  sf::seconds(60.0f / divider).asMicroseconds();
             clock.restart();
         } 
-        ticks = 0;
+        ticks=0;
+        acum=0;
         break;
 
     case 1:
@@ -47,19 +49,43 @@ unsigned Generic_Clock::handleInterrupt()
 
 void Generic_Clock::tick() 
 {
-    if (divider >0) {
 
-        acum += clock.getElapsedTime().asMicroseconds();
+    update(); //new
+    /*if (divider >0) {
+
+        acum += clock.getElapsedTime().asMicroseconds(); //not accurate 
+        //We lose data on windows because clock.getElapsedTime() = 0 
+        //refresh time is to speed ! so we cannot accumulate
         clock.restart();
-
-        if (acum > max_acum ) {
+       if (acum > max_acum ) {
             //std::cerr << "\t\tMax: " << this->max_acum;
             //std::cerr << "  preacum "<< acum << std::endl;
             acum -= max_acum;
             //std::cerr << "  postacum "<< acum << std::endl;
             ticks++;
-            trigger = msg > 0;
+            trigger = msg > 0 ? 1 : 0;
         }
+        
+    }*/
+}
+
+//instead of calling this in ticks we can call this in main loop gain more than
+// 1600 call by loop !
+void Generic_Clock::update()
+{
+    if (divider >0) {
+        //why not ? (it's slow operation)
+        acum = clock.getElapsedTime().asMilliseconds()%max_acum;
+        
+        //hope we have
+        int64_t now = clock.getElapsedTime().asMilliseconds()/max_acum;
+        if (now > ticks)
+        {
+            trigger = msg > 0 ? now - ticks : 0; //no ticks lost !
+            ticks = clock.getElapsedTime().asMilliseconds()/max_acum; 
+        }
+        
+        
     }
 }
 
@@ -71,7 +97,7 @@ bool Generic_Clock::checkInterrupt (uint16_t &msg)
         //std::cerr << delta.count() << " ms" << std::endl;
         //e = std::chrono::high_resolution_clock::now();
 
-        trigger = false;
+        trigger--;
         msg = this->msg;
         return true;
     }
