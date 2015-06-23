9e4555091020832d67a10dd93ef488c248f00361
Luis Panadero Guardeño <luis.panadero@gmail.com>
2015-06-23 19:31:19 +0200

    Updated/Fix build
    Now compiles, at least, on kUbuntu 15.04

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -1,22 +1,22 @@
 if (CMAKE_HOST_APPLE)
-    set(DCPU_VM_SEARCH_PATHS
-        /usr
-        /Applications/Xcode.app/Contents/Developer/Platforms.MacOSX.platform/Developer/SDKs
-        /Library/Frameworks
-        /usr/local
-        /opt/local
-    )
-else (CMAKE_HOST_APPLE)
     # OS X is a Unix, but it's not a normal Unix as far as search paths go.
-    if (CMAKE_HOST_UNIX)
-        set(DCPU_VM_SEARCH_PATHS
-            /usr
-            /usr/local
-            /opt/local
-        )
-    endif (CMAKE_HOST_UNIX)
+    set(DCPU_VM_SEARCH_PATHS
+        /Applications/Xcode.app/Contents/Developer/Platforms.MacOSX.platform/Developer/SDKs
+        ~/Library/Frameworks
+        /Library/Frameworks
+    )
 endif (CMAKE_HOST_APPLE)
-
+set(DCPU_VM_SEARCH_PATHS
+    ${SFML_ROOT}
+    $ENV{SFML_ROOT}
+    /usr/local
+    /usr
+    /sw
+    /opt/local
+    /opt/csw
+    /opt
+    ${DCPU_VM_SEARCH_PATHS}
+)
 
 
 if (MSVC)
@@ -27,14 +27,14 @@ if (MSVC)
 else (MSVC)
     set(PLATFORM_FLAGS "-Wall" ) # Generic flags
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-	  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99")
     if (CMAKE_HOST_WIN32)
       set(DCPU_VM_SEARCH_PATHS
           C:/MinGW
           )
-			
+
     endif (CMAKE_HOST_WIN32)
-    
+
     set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
     set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O3")
 endif (MSVC)
